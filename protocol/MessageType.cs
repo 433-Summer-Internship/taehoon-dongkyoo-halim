c66ad7a09a715052f209f0aec4c09455677ed1b1
lee dong kyoo <dk910320@gmail.com>
2016-08-11 00:23:59 +0900

    Create MessageType.cs

---
+++ protocol/MessageType.cs
@@ -0,0 +1,29 @@
+public class MessageType{
+
+  public const short MSG          = 200; // normal msg  
+  
+  // Membership : 300번대 
+  public const short SIGNUP       = 310; // signup req
+  public const short SIGNIN       = 320; // login req
+  public const short MODIFY = 330; // modify 
+  public const short DELETE = 340;// delete 
+  
+  // Chatting Room : 400번대
+  public const short LIST         = 400; // room list req
+  public const short JOIN         = 410; // join room req
+  public const short LEAVE        = 420; // leave room req
+  public const short CREATE       = 430; // create room req
+  
+  
+  // Admin tool : 500번대 
+  public const short START = 500;
+  public const short SHUTDOWN = 510;
+  public const short RESTART = 520;
+  
+  /*
+  Monitoring 
+  */
+  
+  public const short HEARTBEAT    = 800; // heartbeat
+			
+}
