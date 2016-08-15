c6a113eeaf278c68e8490129278a3034af20c604
lee dong kyoo <dk910320@gmail.com>
2016-08-15 13:32:50 +0900

    Update MessageState.cs

--- protocol/MessageState.cs
+++ protocol/MessageState.cs
@@ -1,8 +1,6 @@
-public class MessageState{
-
-  public const short REQUEST = 100;
-  
-  public const short SUCCESS = 300;
-  public const short FAIL = 400; 
-			
+public enum MessageState
+{
+    Request = 100,
+    Success = 300,
+    Fail = 400
 }
