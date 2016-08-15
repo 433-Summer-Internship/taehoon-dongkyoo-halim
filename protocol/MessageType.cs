6e26c34a981c62b10f72319d3dbfe92d40322ed0
lee dong kyoo <dk910320@gmail.com>
2016-08-15 13:31:33 +0900

    Update MessageType.cs

--- protocol/MessageType.cs
+++ protocol/MessageType.cs
@@ -1,29 +1,28 @@
-public class MessageType{
+public enum MessageType
+{
+    Chatting = 200,
 
-  public const short CHATTING          = 200; // normal msg  
-  
-  // Membership : 300번대 
-  public const short SIGNUP       = 310; // signup req
-  public const short SIGNIN       = 320; // login req
-  public const short MODIFY = 330; // modify 
-  public const short DELETE = 340;// delete 
-  
-  // Chatting Room : 400번대
-  public const short LIST         = 400; // room list req
-  public const short JOIN         = 410; // join room req
-  public const short LEAVE        = 420; // leave room req
-  public const short CREATE       = 430; // create room req
-  
-  
-  // Admin tool : 500번대 
-  public const short START = 500;
-  public const short SHUTDOWN = 510;
-  public const short RESTART = 520;
-  
-  /*
-  Monitoring 
-  */
-  
-  public const short HEARTBEAT    = 800; // heartbeat
-			
+    // Membership : 300번대 
+    Signup = 310,
+    Signin = 320,
+    Modify = 330,
+    Delete = 340,
+
+    // Chatting Room : 400번대
+    List = 400,
+    Join = 410,
+    Leave = 420,
+    Create = 430,
+
+    // Admin tool : 500번대 
+    StartApp = 500,
+    ShutdownApp = 510,
+    RestartApp = 520,
+
+    /*
+    Monitoring 
+    */
+
+    // Heartbeat 
+    Heartbeat = 800
 }
