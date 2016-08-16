2c6aee11b228d5ea5c6fcab87fe89fcf72c3c52d
lee dong kyoo <dk910320@gmail.com>
2016-08-16 09:03:05 +0900

    Update CCHeader.cs

--- protocol/client-chat/CCHeader.cs
+++ protocol/client-chat/CCHeader.cs
@@ -1,28 +1,29 @@
-   public struct CCHeader : Header
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunkerLibrary.common.protocol
+{
+    public struct CCHeader : Header
     {
         MessageType type;
         MessageState state;
         int bodyLength;
-        // Cookie cookie;
+        Cookie cookie;
+        UserInfo userInfo;
 
-        
-                public CCHeader(MessageType type, MessageState state, int bodyLength)
-                {
-                    this.type = type;
-                    this.state = state;
-                    this.bodyLength = bodyLength;
-
-                }
-        
-        /*
-        public CCHeader(MessageType type, MessageState state, int bodyLength, Cookie cookie)
+        public CCHeader(MessageType type, MessageState state, int bodyLength, Cookie cookie, UserInfo userInfo)
         {
             this.type = type;
             this.state = state;
             this.bodyLength = bodyLength;
             this.cookie = cookie;
+            this.userInfo = userInfo;
+
         }
-        */
+        
 
         public MessageState State
         {
@@ -36,12 +37,24 @@
             set { type = value; }
         }
 
-        /*
-         * public Cookie Cookie
+        public int BodyLength
+        {
+            get { return bodyLength; }
+            set { bodyLength = value; }
+        }
+        
+        public Cookie Cookie
         {
             get { return cookie; }
             set { cookie = value; }
         }
-        */
+        
+        public UserInfo UserInfo
+        {
+            get { return userInfo; }
+            set { userInfo = value; }
+        }
 
     }
+
+}
