dfdd4c8d4539b113d51410620b541e62276d8a00
lee dong kyoo <dk910320@gmail.com>
2016-08-18 08:38:06 +0900

    Update ServerInfo.cs

--- protocol/ServerInfo.cs
+++ protocol/ServerInfo.cs
@@ -1,6 +1,25 @@
-    public struct ServerInfo
+public struct ServerInfo
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    char[] domain;//15
+    int port;
+
+    public ServerInfo(char[] domain, int port)
     {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
-        char[] ip;//15
-        int port;
+        this.domain = domain;
+        this.port = port;
     }
+
+    public char[] Domain
+    {
+        get { return this.domain; }
+        set { domain = value; }
+    }
+
+    public int Port
+    {
+        get { return this.port; }
+        set { port = value; }
+    }
+
+}
