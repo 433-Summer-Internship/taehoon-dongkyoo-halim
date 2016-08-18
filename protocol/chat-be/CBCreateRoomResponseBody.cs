55706d14552ffb8efa8142f1f2880d2a062f9648
lee dong kyoo <dk910320@gmail.com>
2016-08-18 20:45:43 +0900

    Update CBCreateRoomResponseBody.cs

--- protocol/chat-be/CBCreateRoomResponseBody.cs
+++ protocol/chat-be/CBCreateRoomResponseBody.cs
@@ -1,4 +1,17 @@
-public struct CBCreateRoomResponseBody{
-  UserInfo userInfo;
-  ChattingRoom chattingRoom;
+using LunkerLibrary.common.protocol;
+
+public struct CBCreateRoomResponseBody : Body
+{
+    //UserInfo userInfo;
+    ChattingRoom chattingRoom;
+
+    public CBCreateRoomResponseBody(ChattingRoom chattingRoom)
+    {
+        this.chattingRoom = chattingRoom;
+    }
+
+    public ChattingRoom ChattingRoom{
+        get { return chattingRoom; }
+        set { chattingRoom = value; }
+    }
 }
