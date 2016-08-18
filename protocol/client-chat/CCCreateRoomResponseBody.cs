f216aea3e9438d0064b5f53f7971c89b00ae4772
lee dong kyoo <dk910320@gmail.com>
2016-08-18 20:52:22 +0900

    Update CCCreateRoomResponseBody.cs

--- protocol/client-chat/CCCreateRoomResponseBody.cs
+++ protocol/client-chat/CCCreateRoomResponseBody.cs
@@ -1,3 +1,15 @@
-public struct CCCreateRoomResponseBody : Body{
-  ChattingRoom chattingRoom;
+public struct CCCreateRoomResponseBody : Body
+{
+    ChattingRoom chattingRoom;
+
+    public CCCreateRoomResponseBody(ChattingRoom chattingRoom)
+    {
+        this.chattingRoom = chattingRoom;
+    }
+
+    public ChattingRoom ChattingRoom
+    {
+        get { return chattingRoom; }
+        set { chattingRoom = value; }
+    }
 }
