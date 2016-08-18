bda35e13d5bcd50cdadcd12f62738809ba2bc7a5
lee dong kyoo <dk910320@gmail.com>
2016-08-18 10:22:20 +0900

    Update CLSigninResponseBody.cs

--- protocol/client-login/CLSigninResponseBody.cs
+++ protocol/client-login/CLSigninResponseBody.cs
@@ -1,4 +1,26 @@
-public struct CLSigninResponseBody{
-  Cookie cookie;
-  ServerInfo serverInfo;
+using LunkerLibrary.common.protocol;
+
+public struct CLSigninResponseBody
+{
+    Cookie cookie;
+    ServerInfo serverInfo;
+
+    public CLSigninResponseBody(Cookie cookie, ServerInfo serverInfo)
+    {
+        this.cookie = cookie;
+        this.serverInfo = serverInfo;
+    }
+
+    public Cookie Cookie
+    {
+        get { return cookie; }
+        set { cookie = value; }
+    }
+
+    public ServerInfo ServerInfo
+    {
+        get { return serverInfo; }
+        set { serverInfo = value; }
+    }
+
 }
