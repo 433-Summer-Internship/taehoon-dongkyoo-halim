2cf6b6b326d1e9a1e7bebdc2e19213ebdd306621
lee dong kyoo <dk910320@gmail.com>
2016-08-18 20:41:31 +0900

    Update CCJoinRequestBody.cs

--- protocol/client-chat/CCJoinRequestBody.cs
+++ protocol/client-chat/CCJoinRequestBody.cs
@@ -1,3 +1,14 @@
-public struct CCJoinRequestBody : Body{
-  int roomNo;
+public struct CCJoinRequestBody : Body
+{
+    ChattingRoom roomInfo;
+    public CCJoinRequestBody(ChattingRoom roomInfo)
+    {
+        this.roomInfo = roomInfo;
+    }
+
+    public ChattingRoom RoomInfo
+    {
+        get { return this.roomInfo; }
+        set { roomInfo = value; }
+    }
 }
