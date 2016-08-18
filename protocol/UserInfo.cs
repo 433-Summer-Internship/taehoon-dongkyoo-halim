89a4a377dc96a8cf17ea52f9198cbe5d49617327
Ha Lim Lee <eblikes2play@gmail.com>
2016-08-18 17:39:11 +0900

    Update UserInfo.cs

--- protocol/UserInfo.cs
+++ protocol/UserInfo.cs
@@ -2,5 +2,7 @@ public struct UserInfo{
   [MarshalAs(UnmanagedType.ByValArray, SizeConst = 18)] 
   char[] id;
   [MarshalAs(UnmanagedType.ByValArray, SizeConst = 18)] 
-  char[] pwd;
+  char[] pwd;  
+  [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] 
+  bool isDummy;  
 }
