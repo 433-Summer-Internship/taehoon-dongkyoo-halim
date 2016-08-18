891843acc452a5d7b5fee6f7e63726ff6dbd82ee
lee dong kyoo <dk910320@gmail.com>
2016-08-18 20:51:10 +0900

    Update CCJoinResponseBody.cs

--- protocol/client-chat/CCJoinResponseBody.cs
+++ protocol/client-chat/CCJoinResponseBody.cs
@@ -1,7 +1,22 @@
-public struct CCJoinResponseBody : Body{
-  // If Success
-  // none
-  
-  // If Fail
-  ServerInfo serverInfo;
+using LunkerLibrary.common.protocol;
+
+public struct CCJoinResponseBody : Body
+{
+    // If Success
+    // none
+
+    // If Fail
+    ServerInfo serverInfo;
+
+    public CCJoinResponseBody(ServerInfo serverInfo)
+    {
+        this.serverInfo = serverInfo;
+    }
+
+    public ServerInfo ServerInfo
+    {
+        get { return serverInfo; }
+        set { serverInfo = value; }
+    }
+
 }
