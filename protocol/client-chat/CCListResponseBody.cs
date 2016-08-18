6422526e475efa1076d94362ffa13f07a0b2f302
lee dong kyoo <dk910320@gmail.com>
2016-08-18 21:05:21 +0900

    Update CCListResponseBody.cs

--- protocol/client-chat/CCListResponseBody.cs
+++ protocol/client-chat/CCListResponseBody.cs
@@ -1,3 +1,18 @@
-public struct CCListResponseBody : Body{
-  ChattingRoom[] chattingRoomList;
+
+public struct CCListRoomResponseBody : Body
+{
+    ChattingRoom[] chattingRoomList;
+
+
+    public CCListRoomResponseBody(ChattingRoom[] chattingRoomList)
+    {
+        this.chattingRoomList = chattingRoomList;
+    }
+
+    public ChattingRoom[] ChattingRoomList
+    {
+        get { return chattingRoomList; }
+        set { chattingRoomList = value; }
+    }
+
 }
