a86eed78b89edbea8b684f1f1528d030c72c3e0b
lee dong kyoo <dk910320@gmail.com>
2016-08-18 20:49:27 +0900

    Update CCLeaveRequestBody.cs

--- protocol/client-chat/CCLeaveRequestBody.cs
+++ protocol/client-chat/CCLeaveRequestBody.cs
@@ -1,3 +1,16 @@
-public struct CCLeaveRequestBody : Body{
-  int roomNumber;
+public struct CCLeaveRequestBody : Body
+{ 
+    ChattingRoom roomInfo;
+    public CCLeaveRequestBody(ChattingRoom roomInfo)
+    {
+        this.roomInfo = roomInfo;
+    }
+
+    public ChattingRoom RoomInfo
+    {
+        get { return this.roomInfo; }
+        set { roomInfo = value; }
+    }
+
+
 }
